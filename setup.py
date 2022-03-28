866ff12e6ef0fca188c96ca755d6238d58cdd781
Fabian665 <fabian665@gmail.com>
2022-03-28 16:26:00 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+import zipfile
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+if not os.path.isdir('data'):
+    os.mkdir('data')
+elif os.path.isfile(os.path.join('data', 'Test.csv')):
+    api = KaggleApi()
+    api.authenticate()
+
+    api.dataset_download_cli('meowmeowmeowmeowmeow/gtsrb-german-traffic-sign')
+
+    zip_path = os.path.join(api.get_default_download_dir(), 'gtsrb-german-traffic-sign.zip')
+    if not os.path.isdir('data'):
+        os.mkdir('data')
+
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall('data')
