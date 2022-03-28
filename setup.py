4e121ee9e588ed38f1185105afba0a17502523f6
Fabian665 <fabian665@gmail.com>
2022-03-28 16:40:20 +0300

    Added print statements and 'not' to if test exists

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from kaggle.api.kaggle_api_extended import KaggleApi
 if not os.path.isdir('data'):
     os.mkdir('data')
 
-if os.path.isfile(os.path.join('data', 'Test.csv')):
+if not os.path.isfile(os.path.join('data', 'Test.csv')):
     api = KaggleApi()
     api.authenticate()
 
@@ -13,5 +13,7 @@ if os.path.isfile(os.path.join('data', 'Test.csv')):
 
     zip_path = os.path.join(api.get_default_download_dir(), 'gtsrb-german-traffic-sign.zip')
 
+    print('Starting to unzip...')
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall('data')
+    print('unzip finished')
