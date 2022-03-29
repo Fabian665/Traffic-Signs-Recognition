f0d2920f698a62975e1991e12f7c398b48393692
Fabian665 <fabian665@gmail.com>
2022-03-29 16:26:19 +0300

    Moved static functions to utils.py

--- rsd.py
+++ rsd.py
@@ -2,6 +2,7 @@ import os
 import sys
 import cv2
 import imghdr
+import utils
 import numpy as np
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -83,57 +84,6 @@ class RoadSignsDetection:
             else:
                 continue
 
-    @staticmethod
-    def validate_path(input_text: str) -> tuple:
-        if '\\' in input_text:
-            file_path = tuple(input_text.split('\\'))
-        elif '/' in input_text:
-            file_path = tuple(input_text.split('/'))
-        else:
-            file_path = input_text
-
-        if not isinstance(file_path, str):
-            im_path = os.path.join(*file_path)
-        else:
-            im_path = file_path
-
-        if not os.path.isfile(im_path):
-            print(f'{im_path} is not a file or the path is wrong, please try again')
-            return False, None
-        else:
-            return True, im_path
-
-    @staticmethod
-    def get_image(im_path: str):
-        """
-        Get image from path
-        :param im_path: str - Relative or Absolute path
-        :return: tuple of image and image resized
-        """
-        image = cv2.imread(im_path)
-        original_image = image
-        original_image = cv2.resize(original_image, (150, 150))
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = cv2.resize(image, (30, 30))
-        return original_image, image
-
-    @staticmethod
-    def get_images_from_dir(im_path):
-        if not os.path.isdir(im_path):
-            raise OSError(f'{im_path} is not a valid path')
-        data = []
-        for image in os.listdir(im_path):
-            try:
-                if imghdr.what(os.path.join(im_path, image)) is None:
-                    continue
-                image = cv2.imread(os.path.join(im_path, image))
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                image = cv2.resize(image, (30, 30))
-                data.append(image)
-            except PermissionError:
-                continue
-        return data
-
     def predict_one(self, image: np.ndarray):
         image = np.array([image])
         prediction = self.model.predict(image)
@@ -153,13 +103,13 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         path = app.get_input()
     else:
-        res, string = app.validate_path(sys.argv[1])
+        res, string = utils.validate_path(sys.argv[1])
         if res:
             path = string
         else:
             path = app.get_input()
 
-    original_im, im = app.get_image(path)
+    original_im, im = utils.get_image(path)
     pred = app.predict_one(im)
     print(pred)
     cv2.imshow('original', original_im)
